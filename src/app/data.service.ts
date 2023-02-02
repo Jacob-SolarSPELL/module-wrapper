59598f1a97583c0acd1790cf7d8768d5823872d2
Jacob Shaeffer <jmshaeff@asu.edu>
2023-02-02 11:42:17 -0700

    added image to home component on categories

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,48 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Subcategory } from './models/subcategory';
 
+////Dev new//////////////////////////////////////////////////////////////////////////////////////
+const cats = [
+  {
+    id: 11,
+    name: "Test 11",
+    description: "This is a root level folder",
+    path: "/Test 11",
+    has_img: true,
+    has_files: false,
+    parent_id: null,
+  },
+  {
+    id: 12,
+    name: "Test 12",
+    description: "This is also a root level folder",
+    path: "/Test 12",
+    has_img: false,
+    has_files: false,
+    parent_id: null,
+  },
+  {
+    id: 121,
+    name: "subcat test 121",
+    description: "this is a test",
+    path: "/Test 11/subcat test 121",
+    has_img: true,
+    has_files: true,
+    parent_id: 12,
+  }
+];
+
+const fls = [
+  {
+    id: 1,
+    name: "test file",
+    size: "5MB",
+    file_path: "/Test 12/subcat test 121/test file.txt",
+    category_id: 121,
+  }
+];
+////Dev end//////////////////////////////////////////////////////////////////////////////////////
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,23 +59,79 @@ export class DataService {
   }
 
   getCategories(): Observable<any> {
-    return this.http.get(this.apiUrl.concat('categories.php',""));
+////Dev old//////////////////////////////////////////////////////////////////////////////////////
+    // return this.http.get(this.apiUrl.concat('categories.php',""));
+////Dev new//////////////////////////////////////////////////////////////////////////////////////
+    return new Observable((observer) => {
+      var topLevelCats = cats.filter((cat) => cat.parent_id == null);
+      var mapped = topLevelCats.map((cat) => {return {
+        id: cat.id,
+        name: cat.name,
+        description: cat.description,
+        path: cat.path,
+        has_img: cat.has_img,
+        has_files: cat.has_files
+      }});
+      observer.next(mapped);
+    });
+////Dev end//////////////////////////////////////////////////////////////////////////////////////
   }
 
   getSubcategories(parentId:number): Observable<any> {
     let params = new HttpParams();
     params = params.append('parent_id', parentId);
-    return this.http.get(this.apiUrl.concat('subcategory_by_category.php'), {params});
+////Dev old//////////////////////////////////////////////////////////////////////////////////////
+    // return this.http.get(this.apiUrl.concat('subcategory_by_category.php'), {params});
+////Dev new//////////////////////////////////////////////////////////////////////////////////////
+    return new Observable((observer) => {
+      var subCats = cats.filter((cat) => cat.parent_id == parentId);
+      var mapped = subCats.map((cat) => {return {
+        id: cat.id,
+        name: cat.name,
+        has_files: cat.has_files
+      }});
+      observer.next(
+        {
+          parentName: cats.find((cat) => cat.parent_id == parentId)?.name,
+          subcategories: mapped,
+        }
+      );
+    });
+////Dev end//////////////////////////////////////////////////////////////////////////////////////
   }
 
   getFiles(parentId:number): Observable<any> {
     let params = new HttpParams();
     params = params.append('id', parentId);
-    return this.http.get(this.apiUrl.concat('files_by_category.php'), {params});
+////Dev old//////////////////////////////////////////////////////////////////////////////////////
+    // return this.http.get(this.apiUrl.concat('files_by_category.php'), {params});
+////Dev new//////////////////////////////////////////////////////////////////////////////////////
+    var files = fls.filter((file) => file.category_id == parentId);
+    var mapped = files.map((file) => {return {
+      id: file.id,
+      name: file.name,
+      size: file.size,
+      file_path: file.file_path,
+      category_id: file.category_id.toString(),
+      category_name: cats.find((cat) => cat.id == file.category_id)?.name
+    }});
+    return new Observable((observable) => {
+      observable.next({
+        parentName: cats.find((cat) => cat.parent_id == parentId)?.name,
+        files: mapped,
+      });
+    });
+////Dev end//////////////////////////////////////////////////////////////////////////////////////
   }
 
   getAllFiles(): Observable<any> {
-    return this.http.get(this.apiUrl.concat('files_all.php'));
+////Dev old//////////////////////////////////////////////////////////////////////////////////////
+    // return this.http.get(this.apiUrl.concat('files_all.php'));
+////Dev new//////////////////////////////////////////////////////////////////////////////////////
+    return new Observable((observable) => {
+      observable.next();
+    });
+////Dev end//////////////////////////////////////////////////////////////////////////////////////
   }
 }
 
--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -2,5 +2,7 @@ export class Category {
     id: number;
     name: string;
     description: string;
+    path: string;
+    has_img: boolean;
     has_files: boolean;
 }
